b335066251e85e1e8daa89767e84d5b31643d745
vladimirnetworks <vladimirnetworks@gmail.com>
2022-08-09 20:42:47 +0430

    update

--- src/app/domain-config/domain-config.component.ts
+++ src/app/domain-config/domain-config.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-domain-config',
@@ -8,14 +10,70 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DomainConfigComponent implements OnInit {
 
-  domain:any;
-  constructor(private ar: ActivatedRoute) { 
-    this.ar.parent?.paramMap.subscribe((parent:any)=>{
-    this.domain =  parent["params"]['domain'];
+  domain: any;
+  // conf:Observable<any>
+
+
+  title: string = "";
+  cat: string = "";
+
+  constructor(private ar: ActivatedRoute, private api: ApiService, public router: Router) {
+    this.ar.parent?.paramMap.subscribe((parent: any) => {
+      this.domain = parent["params"]['domain'];
     });
+
+    this.api.get("domain/" + this.domain).subscribe((x: any) => {
+      console.log(x)
+      this.title = x.title;
+      this.cat = x.cats;
+    });
+
+  }
+
+
+
+
+
+  update() {
+    // console.log(this.conf$)
+    this.api.post("domain/" + this.domain, { "title": this.title, "cat": this.cat }).subscribe((x: any) => {
+
+
+
+    }
+    );
+  }
+
+
+  delete() {
+
+    let randx = new Array(10).join().replace(/(.|$)/g, function () { return ((Math.random() * 36) | 0).toString(36)[Math.random() < .5 ? "toString" : "toUpperCase"](); });
+
+
+    let randxrev = [...randx].reverse().join("");
+
+    if (confirm("delete this blog ?")) {
+      alert("keep this in your mind " + randx)
+
+      if (prompt("type that reversed : ") == randxrev && confirm("motmaen ?") && confirm("motmaen2 ?") && confirm("motmaen3 ?") && confirm("motmaen4 ?") && prompt("بنویس له له له له له له له له لم له له لم به انگلیسی") == "la la la la la la la la lam la la lam") {
+        this.api.delete("domain/" + this.domain).subscribe((x: any) => {
+          this.router.navigate(['/console'], { relativeTo: this.ar });
+
+        }
+        );
+      } else {
+        alert("نشد")
+      }
+    }
+
   }
 
   ngOnInit(): void {
   }
 
+}
+
+
+interface configx {
+  title: string;
 }
